91d76cef0591be08a5465ccabec06201b90f9b6d
Xavier Comi <xaviercomi@gmail.com>
2025-04-03 01:59:44 +0200

    Modificacion de los archivos imagenes de portada. substituir espacios i apóstrofes

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ import usuariosRoutes from "./usuariosRoutes.js";
 import interaccionsRoutes from "./interaccionsRoutes.js";
 import citasRoutes from "./citasRoutes.js";
 import rosasRoutes from "./rosasRoutes.js";
+import librosRoutes from "./librosRoutes.js";
 
 const router = express.Router();
 
@@ -10,5 +11,6 @@ router.use("/usuarios", usuariosRoutes);
 router.use("/interacciones", interaccionsRoutes);
 router.use("/citas", citasRoutes);
 router.use("/rosas", rosasRoutes);
+router.use("/libros", librosRoutes);
 
 export default router;
--- routes/librosRoutes.js
+++ routes/librosRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 
-import { getLibros } from "../controller/librosController.js";
+import { getLibros } from "../controllers/librosController.js";
 
 const router = express.Router();
 
