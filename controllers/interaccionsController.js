7991fb91e91c26526979771f7fedaa7a6416e004
Xavier Comi <xaviercomi@gmail.com>
2025-04-04 21:20:56 +0200

    Modificación del controller interaccion para agregar uuid y query id_usuario

--- controllers/interaccionsController.js
+++ controllers/interaccionsController.js
@@ -16,27 +16,21 @@ export const getAllInteracciones = async (req, res) => {
 // Agregar una nueva interacción
 export const addInteraccion = async (req, res) => {
   try {
-    const {
-      usuario_id,
-      destinatario_nombre,
-      cita_id,
-      rosa_id,
-      libro_titulo,
-      libro_autor,
-      libro_imagen_url,
-    } = req.body;
+    const { uuid, destinatario_nombre, cita_id, rosa_id, libro_id } = req.body;
+
+    const userId = await pool.query("SELECT id FROM usuario WHERE uuid = $1", [
+      uuid,
+    ]);
+
+    if (userId.rows.length === 0) {
+      return res.status(404).json({ error: "Usuario no encontrado" });
+    }
+
+    const usuario_id = userId.rows[0].id;
 
     const result = await pool.query(
-      "INSERT INTO interaccion (usuario_id, destinatario_nombre, cita_id, rosa_id, libro_titulo, libro_autor, libro_imagen_url) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING * ",
-      [
-        usuario_id,
-        destinatario_nombre,
-        cita_id,
-        rosa_id,
-        libro_titulo,
-        libro_autor,
-        libro_imagen_url,
-      ]
+      "INSERT INTO interaccion (usuario_id, destinatario_nombre, cita_id, rosa_id, libro_id) VALUES($1, $2, $3, $4, $5) RETURNING * ",
+      [usuario_id, destinatario_nombre, cita_id, rosa_id, libro_id]
     );
     res.status(201).json(result.rows[0]);
   } catch (error) {
