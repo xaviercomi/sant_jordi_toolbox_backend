e1a85500a49e8115e93dfb47d523fe40de5dd121
Xavier Comi <xaviercomi@gmail.com>
2025-04-08 16:07:29 +0200

    Habilitando SSL en DB

--- config/db.js
+++ config/db.js
@@ -12,6 +12,9 @@ const pool = new Pool({
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT || 5432,
+  ssl: {
+    rejectUnautorized: false,
+  },
 });
 
 export default pool;
