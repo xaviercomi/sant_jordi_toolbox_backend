34fdd9e48e3195d8f62fcb374103c001cd6f097f
Xavier Comi <xaviercomi@gmail.com>
2025-04-17 16:28:47 +0200

    Controlador y ruta de librerias, Instalacion axios

---
+++ controllers/libreriasController.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+
+export const getLibrerias = async (req, res) => {
+  try {
+    const { lat, lng } = req.query;
+
+    if (!lat || !lng) {
+      return res.status(400).json({ error: "Faltan coordenadas lat/lng" });
+    }
+
+    const GOOGLE_API_KEY = process.env.GOOGLE_MAPS_API_KEY;
+
+    const response = await axios.get(
+      "https://maps.googleapis.com/maps/api/place/nearbysearch/json",
+      {
+        params: {
+          location: `${lat},${lng}`,
+          radius: 6000,
+          keyword: "bookstore",
+          key: GOOGLE_API_KEY,
+        },
+      }
+    );
+
+    const librerias = response.data.results.map((place) => ({
+      id: place.place_id,
+      name: place.name,
+      latitude: place.geometry.location.lat,
+      longitude: place.eometry.location.lng,
+    }));
+
+    res.json(librerias);
+  } catch (error) {
+    console.error("Error al consultar Google Places:", error.message);
+    res.status(500).json({ error: "Error al obtener lliberies" });
+  }
+};
