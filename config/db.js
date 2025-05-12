1c03a6ada0464f96f6994431761d7eb98465042f
Xavier Comi <xaviercomi@gmail.com>
2025-05-12 11:59:24 +0200

    Migración base de datos a Neon

--- config/db.js
+++ config/db.js
@@ -13,7 +13,7 @@ const pool = new Pool({
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT || 5432,
   ssl: {
-    rejectUnauthorized: false,
+    rejectUnauthorized: true,
   },
 });
 
